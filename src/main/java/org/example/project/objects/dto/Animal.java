2c9ccb052f64c9f3f081845b44d43d9e0d2e6d3f
Ivan Poletsky <wiperus55@gmail.com>
2020-07-26 20:12:29 +0600

    Removed setters Added final keyword to all properties of Animal.java

--- src/main/java/org/example/project/objects/dto/Animal.java
+++ src/main/java/org/example/project/objects/dto/Animal.java
@@ -1,10 +1,11 @@
 package org.example.project.objects.dto;
 
 public class Animal {
-	private String name;
-	private String weight;
-	private String growth;
-	private String type;
+
+	private final String name;
+	private final String weight;
+	private final String growth;
+	private final String type;
 
 	public Animal(String name, String weight, String growth, String type) {
 		this.name = name;
@@ -17,33 +18,19 @@ public class Animal {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getWeight() {
 		return weight;
-	}
-
-	public void setWeight(String weight) {
-		this.weight = weight;
 	}
 
 	public String getGrowth() {
 		return growth;
 	}
 
-	public void setGrowth(String growth) {
-		this.growth = growth;
-	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
 
 	@Override
 	public String toString() {
